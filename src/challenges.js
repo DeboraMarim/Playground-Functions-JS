971391c7b03a53aa5c70d79e88dba155eb2f3b03
Débora Marim <deboramarim77@hotmail.com>
2023-06-16 12:34:20 -0300

    Resolving Playground Functions JS

--- src/challenges.js
+++ src/challenges.js
@@ -1,56 +1,143 @@
-// Desafio 1
-function compareTrue() {
-  // seu código aqui
+// Desafio 1  CONFERIDO!!
+
+function compareTrue(booleana1, booleana2) {
+  if (booleana1 && booleana2) {
+    return true;
+  }
+  return false;
 }
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
+// Desafio 2  CONFERIDO!!
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
+// Desafio 3 CONFERIDO!!
+function splitSentence(stringrecebida) {
+  let vetor = [];
+  let palavra = '';
+  for (let item of stringrecebida) {
+    if (item === ' ') {
+      vetor.push(palavra);
+      palavra = '';
+    } else {
+      palavra += item;
+    }
+  }
+  vetor.push(palavra);
+  return vetor;
 }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
+// Desafio 4 CONFERIDO!!
+function concatName(vetordeNomes){
+  let ultimoNome = vetordeNomes[vetordeNomes.length-1];
+  let primeiroNome = vetordeNomes[0];
+  return ultimoNome +', '+ primeiroNome;
 }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+// Desafio 5 CONFERIDO!!
+function footballPoints(wins,ties) {
+  return wins * 3 + ties;
 }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+// Desafio 6 CONFERIDO!!
+function highestCount(vetor){
+  let maior = vetor[0];
+  for(let index in vetor){
+   if(vetor[index] > maior){
+    maior = vetor[index];
+    } 
+  }
+  let contador = 0;
+  for(let index in vetor){
+    if(vetor[index] === maior){
+      contador += 1;
+    }
+  }
+return contador;
 }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+// Desafio 7  CONFERIDO!!
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaGato1 = Math.abs(cat1 - mouse);
+  let distanciaGato2 = Math.abs(cat2 - mouse);
+
+  if(distanciaGato1 > distanciaGato2){
+    return 'cat2';
+  }else if(distanciaGato2 > distanciaGato1){
+    return 'cat1'
+  }else return ('os gatos trombam e o rato foge');
 }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+
+// Desafio 8 CONFERIDO!!
+function fizzBuzz(vetor) {
+  let resul = [];
+  for (let index = 0; index < vetor.length; index += 1) {
+    if (vetor[index] % 3 === 0 && vetor[index] % 5 === 0) {
+      resul.push('fizzBuzz');
+    } else if (vetor[index] % 5 === 0) {
+      resul.push('buzz');
+    } else if (vetor[index] % 3 === 0) {
+      resul.push('fizz');
+    } else {
+      resul.push('bug!');
+    }
+  }
+  return resul;
 }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
 
+// Desafio 9  CONFERIDO!!
+
+let charr = {
+  a: 1,
+  e: 2,
+  i: 3,
+  o: 4,
+  u: 5,
+};
+function encode(palavra) {
+  let palavraCodificada = '';
+  let letraN = '';
+  for (let letra of palavra) {
+    letraN = letra;
+    for (let cod in charr) {
+      if (letra === cod) {
+        letraN = charr[cod];
+      }
+    }
+    palavraCodificada += letraN;
+  }
+  return palavraCodificada;
+}
+function decode(palavra) {
+  let palavraDecodificada = '';
+  let letraN = '';
+  for (let letra of palavra) {
+    letraN = letra;
+    for (let cod in charr) {
+      if (letra === charr[cod].toString()) {
+        letraN = cod;
+      }
+    }
+    palavraDecodificada += letraN;
+  }
+  return palavraDecodificada;
+}
+// Desafio 10  CONFERIDO!!
+function techList(vetor, nome) {
+  if (vetor.length === 0) {
+    return 'Vazio!';
+  }
+  let vetorN = [];
+  for (let item of vetor.sort()) {
+    let do1 = { tech: item, name: nome };
+    vetorN.push(do1);
+  }
+  return vetorN;
+}
 module.exports = {
   calcArea,
   catAndMouse,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,20 +1,58 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(array) {
+  let verificador = [0,0,0,0,0,0,0,0,0,0];
+  if( array.length != 11 ) {
+    return 'Array com tamanho incorreto.'
+  }else{
+    for(let index = 0; index < 11; index +=1)
+      if( array[index] <0 || array[index] > 9 ) 
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      for(let index = 0; index < 11; index +=1){
+            let ponteiro = array[index];
+            verificador[ponteiro]++;
+          }
+       for(let index = 0; index < 11; index +=1){
+        if(verificador[index]>=3)
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+       let numeroFormatado = ('('+array[0]+array[1]+') '+array[2]+array[3]+array[4]+array[5]+array[6]+'-'+array[7]+array[8]+array[9]+array[10]); 
+       return numeroFormatado;
+
+      }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < (lineB + lineC) && lineA > (Math.abs(lineB - lineC))) {
+    if (lineB < (lineA + lineC) && lineB > (Math.abs(lineA - lineC))) {
+      if (lineC < (lineA + lineB) && lineC > (Math.abs(lineA - lineB))) {
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
+  return false;
 }
 
+
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(entrada) {
+  let inf = /\d+/g;
+  let vetor = entrada.match(inf);
+  let x = 0;
+
+  for (let i = 0; i < vetor.length; i += 1) {
+    x += Number(vetor[i]);
+  }
+
+  return `${x} copo${x > 1 ? 's' : ''} de água`;
 }
+
 
 module.exports = {
   generatePhoneNumber,
   hydrate,
   triangleCheck,
 };
+    
